fc8f924562596d3344880a190baae02d0563cfa4
管鐘 <t126017977@gmail.com>
2023-12-29 00:07:40 +0800

    New version

--- app/App.py
+++ app/App.py
@@ -5,7 +5,7 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
-# from openai import OpenAI, api_key
+from openai import OpenAI, api_key
 from sqlalchemy import text
 from pprint import pprint
 
@@ -16,8 +16,8 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
 
 db = SQLAlchemy(app)
 
-# client_gpt = OpenAI()
-# api_key = os.getenv("OPENAI_API_KEY")
+client_gpt = OpenAI()
+api_key = os.getenv("OPENAI_API_KEY")
 
 # 這裡要引入 models，db 才會知道有哪些 table 要建置
 from models import *
